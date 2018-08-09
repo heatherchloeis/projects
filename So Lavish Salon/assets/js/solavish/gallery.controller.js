f8a620d2ce7e91f0b7a8b072bcfa1bade8ade10a
Heather Chloe Wallace <heatherchloeis@gmail.com>
2018-08-09 15:20:31 -0700

    modified image tags

--- So Lavish Salon/assets/js/solavish/gallery.controller.js
+++ So Lavish Salon/assets/js/solavish/gallery.controller.js
@@ -8,14 +8,14 @@
 		$scope.images = {};
 
 		for (var i = 1; i < 42; i++) {
-			$scope.images[i - 1] = { 'url': '/assets/images/gallery/instagram' + i + '.jpg'};
+			$scope.images[i - 1] = { 'url': '../images/gallery/instagram' + i + '.jpg'};
 		}
 
 		$('#galleryModal').on('show.bs.modal', function(event) {
 			var button = $(event.relatedTarget);
 			var idx = button.data('index') + 1;
 			console.log("current image is " + idx );
-			$('#galleryImage').attr('src', '/assets/images/gallery/instagram' + idx + '.jpg');
+			$('#galleryImage').attr('src', '../images/gallery/instagram' + idx + '.jpg');
 		});
 	});
 })();
